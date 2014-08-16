e8008549718e3b846f9c35ae81424f4eb59399ac
jake <jake@jakecoffman.com>
2014-08-16 15:53:21 -0500

    Added db stuff and made index page less ugly.

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "db.sqlite")
+	check(err)
+	defer db.Close()
+
+	initDb(db)
+
 	http.Handle("/static/", static)
 	http.HandleFunc("/", filter(handle))
 
 	host := "localhost:5001"
 	log.Println("Serving on " + host)
 	log.Fatal(http.ListenAndServe(host, nil))
+}
+
+// idempotent bootstrap
+func initDb(db *sql.DB) {
+	s := `create table if not exists users (id integer not null primary key, name text, password text);`
+	_, err := db.Exec(s)
+	check(err)
+
+	rows, err := db.Query("select id, name from users where name='admin'")
+	check(err)
+	defer rows.Close()
+	if !rows.Next() {
+		_, err = db.Exec("insert into users values(0, 'admin', 'admin')")
+		check(err)
+	}
 }
 
 var static http.Handler = http.StripPrefix("/static", http.FileServer(http.Dir("static")))
@@ -47,4 +71,10 @@ func handle(w http.ResponseWriter, r *http.Request) int {
 	t.Execute(w, nil)
 	log.Println(r.RemoteAddr, r.URL, 200)
 	return 200
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
